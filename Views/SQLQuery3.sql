7d61253d17b4c689cc45cf6cd9685a56e36d1097
amireza <amireza.vishteh@gmail.com>
2022-07-02 22:23:43 +0430

    addconstaint

--- Views/SQLQuery3.sql
+++ Views/SQLQuery3.sql
@@ -1,7 +1,7 @@
-Create View clothe_customer_order
-AS
-SELECT (C.firstname+C.lastname) fullname ,O.o_count countoforder , (cl.price*O.o_count) toatal_price
-From customer C join C_order O on  C.ID= O.customer_ID   join Clothe cl On cl.clothe_ID=O.cloth_ID
+create view branch_bmanager_customerclub_centeraloffice
+As
+SELECT B.ID BranchID , BM.username MANAGERNAME , Cf.office_mager_username OFFICEUSERNAME , CC.ID CustomerCLUBID
+From Branch B Join Branch_manager BM ON B.branch_manager_ID=BM.ID join central_office Cf On Cf.Office_manager_ID=BM.office_manager_ID join customer_club CC ON CC.central_office_ID=Cf.ID
 
 
 
